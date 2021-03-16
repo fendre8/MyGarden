30bb2c0821f38468224f4b54354df89d6dfe84ab
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-03-16 11:17:24 +0100

    change project

--- backend/MyGarden/Startup.cs
+++ backend/MyGarden/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MyGarden.DAL.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,9 @@ namespace MyGarden
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyGarden", Version = "v1" });
             //});
+
+            services.AddDbContext<MyGardenDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MyGardenDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
