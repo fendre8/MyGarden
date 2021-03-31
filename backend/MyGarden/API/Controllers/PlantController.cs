6d406a55f33de967c28e48726fc3c11c95b2db00
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-03-31 09:18:36 +0200

    Add plant controller

---
+++ backend/MyGarden/API/Controllers/PlantController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyGarden.DAL;
+using MyGarden.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGarden.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantController : ControllerBase
+    {
+        private readonly IPlantsRepository repository;
+
+        public PlantController(IPlantsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Plant>> List()
+        {
+            return await repository.List();
+        }
+
+
+    }
+}
