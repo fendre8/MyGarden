5505ec708e76331fb8be0b0658d7fdc77b108442
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-03-31 09:19:24 +0200

    Services

--- backend/MyGarden/BLL/Services/AuthService.cs
+++ backend/MyGarden/BLL/Services/AuthService.cs
@@ -1,12 +1,29 @@
 ﻿using MyGarden.BLL.Services.Interfaces;
+using MyGarden.DAL.EF.DbModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyGarden.BLL.Services
 {
     public class AuthService : IAuthService
     {
+        public Guid CreateNewUser(RegisterModel userRegistration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyPassword(string actualPassword, string hashedPassword)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- backend/MyGarden/BLL/Services/Interfaces/IAuthService.cs
+++ backend/MyGarden/BLL/Services/Interfaces/IAuthService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyGarden.DAL.EF.DbModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace MyGarden.BLL.Services.Interfaces
 {
     interface IAuthService
     {
+        ApplicationUser GetUserByEmail(string email);
+        Guid CreateNewUser(RegisterModel userRegistration);
+        string HashPassword(string password);
+        bool VerifyPassword(string actualPassword, string hashedPassword);
     }
 }
