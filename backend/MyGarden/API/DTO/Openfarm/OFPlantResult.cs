961fec3db9d1267ff2723b4c4506f108d189cb3a
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-07 22:42:32 +0200

    Openfarm integration

---
+++ backend/MyGarden/API/DTO/Openfarm/OFPlantResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGarden.API.DTO
+{
+    public class OFPlantResult
+    {
+        public Data[] data { get; set; }
+    }
+
+    public class Data
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public Attributes attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        public string name { get; set; }
+        public string slug { get; set; }
+        public string binomial_name { get; set; }
+        public string description { get; set; }
+        public string sun_requirements { get; set; }
+        public string sowing_method { get; set; }
+        public int? spread { get; set; }
+        public int? row_spacing { get; set; }
+        public int? height { get; set; }
+        public string main_image_path { get; set; }
+        public int? growing_degree_days { get; set; }
+    }
+}
---
+++ backend/MyGarden/API/DTO/Openfarm/OpenfarmLoginResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGarden.API.DTO.Openfarm
+{
+
+    public class OpenfarmLoginResult
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public Attributes attributes { get; set; }
+        public Links links { get; set; }
+    }
+
+    public class Attributes
+    {
+        public DateTime expiration { get; set; }
+        public string secret { get; set; }
+    }
+
+    public class Links
+    {
+        public Self self { get; set; }
+    }
+
+    public class Self
+    {
+        public string api { get; set; }
+        public string website { get; set; }
+    }
+
+}
