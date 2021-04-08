c3e6dbfd40961e99879abb8e573b464be498c49a
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-08 19:43:08 +0200

    update

--- backend/MyGarden/Models/Profile.cs
+++ backend/MyGarden/Models/Profile.cs
@@ -15,7 +15,7 @@ namespace MyGarden.Models
         public string Email { get; set; }
         public Image Image { get; set; }
 
-        public List<FriendshipDTO> Friends { get; set; } = new List<FriendshipDTO>();
+        public List<string> Friends { get; set; } = new List<string>();
         public ICollection<string> Plants { get; set; } = new List<string>();
         public ICollection<string> Issues { get; set; } = new List<string>();
 
