24d420a794c1112fc7e0d07a42b25d1e7a1f05be
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-12 15:18:55 +0200

    not use mapper

--- backend/MyGarden/API/DTO/Mapping/MappingProfile.cs
+++ backend/MyGarden/API/DTO/Mapping/MappingProfile.cs
@@ -7,9 +7,6 @@ namespace MyGarden.Models.Mapping
         public MappingProfile()
         {
             CreateMap<DAL.EF.DbModels.ApplicationUser, Profile>();
-            CreateMap<DAL.EF.DbModels.Friendship, FriendshipDTO>()
-                .ForMember(dest => dest.Friend1, source => source.MapFrom(source => source.Friend1.UserName))
-                .ForMember(dest => dest.Friend2, source => source.MapFrom(source => source.Friend2.UserName));
         }
     }
 }
