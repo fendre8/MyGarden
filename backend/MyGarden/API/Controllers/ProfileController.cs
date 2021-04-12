20c95fcc1551e1a1b5a85d562974d49950f7df94
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-12 15:18:30 +0200

    Add plant_repository +AddPlantToGarden

--- backend/MyGarden/API/Controllers/ProfileController.cs
+++ backend/MyGarden/API/Controllers/ProfileController.cs
@@ -16,25 +16,27 @@ namespace MyGarden.API.Controllers
     [ApiController]
     public class ProfileController : ControllerBase
     {
-        private readonly IProfilesRepository repository;
+        private readonly IProfilesRepository profile_repository;
+        private readonly IPlantsRepository plant_repository;
 
-        public ProfileController(IProfilesRepository repository)
+        public ProfileController(IProfilesRepository prof_repository, IPlantsRepository plant_repository)
         {
-            this.repository = repository;
+            this.profile_repository = prof_repository;
+            this.plant_repository = plant_repository;
         }
 
         [HttpGet]
-        public IEnumerable<Profile> List()
+        public async Task<IEnumerable<Profile>> List()
         {
-            return repository.List();
+            return await profile_repository.List();
         }
 
         [HttpGet("id/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Profile> Get(int id)
+        public async Task<ActionResult<Profile>> Get(int id)
         {
-            var value = repository.FindById(id);
+            var value = await profile_repository.FindById(id);
             if (value == null)
                 return NotFound();
             else
@@ -44,38 +46,38 @@ namespace MyGarden.API.Controllers
         [HttpGet("name/{username}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Profile> GetByUsername(string username)
+        public async Task<ActionResult<Profile>> GetByUsername(string username)
         {
-            var value = repository.FindByUserName(username);
+            var value = await profile_repository.FindByUserName(username);
             if (value == null)
                 return NotFound();
             else
                 return Ok(value);
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(StatusCodes.Status201Created)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //public ActionResult<Profile> Create([FromBody] DAL.EF.DbModels.ApplicationUser value)
-        //{
-        //    try
-        //    {
-        //        var created = repository.Insert(value);
-        //        return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
-        //    }
-        //    catch (ArgumentException ex)
-        //    {
-        //        return BadRequest(new { error = ex.Message });
-        //    }
-        //}
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Profile>> Create([FromBody] DAL.EF.DbModels.ApplicationUser value)
+        {
+            try
+            {
+                var created = await profile_repository.Insert(value);
+                return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
 
         [HttpDelete("{id}")]
         [Authorize(Roles ="Admin")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            var task = repository.Delete(id);
+            var task = await profile_repository.Delete(id);
             if (task == null)
                 return NotFound();
             else return NoContent();
@@ -84,9 +86,9 @@ namespace MyGarden.API.Controllers
         [HttpPost("friend")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult AddFriend([FromBody] AddFriendModel friendModel)
+        public async Task<ActionResult> AddFriend([FromBody] AddFriendModel friendModel)
         {
-            var friendship = repository.AddFriend(friendModel.username1, friendModel.username2);
+            var friendship = await profile_repository.AddFriend(friendModel.FriendFrom, friendModel.FriendTo);
             if (friendship == null)
             {
                 return BadRequest();
@@ -95,5 +97,18 @@ namespace MyGarden.API.Controllers
                 return Created(nameof(AddFriend),friendship);
         }
 
+        [HttpPost("name/{username}/{plantName}")]
+        public async Task<ActionResult> AddPlantToGarden([FromRoute] string username, string plantName)
+        {
+            var plant = await plant_repository.AddPlant(plantName, username);
+            if (plant != null)
+            {
+                return Created(nameof(AddPlantToGarden), plant);
+            }
+            else
+                return BadRequest();
+        }
+
     }
+
 }
