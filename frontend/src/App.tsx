c0ca9d9daf49e3a027908dc176ecbb814ae76709
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-28 09:19:07 +0200

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import {
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import MainPage from "./Pages/MainPage";
-import MyNavbar from "./Components/MyNavbar";
 
 const App: React.FC = () => {
   return (
@@ -15,7 +14,6 @@ const App: React.FC = () => {
       <Router>
         <Switch>
           <Route exact path="/">
-            <MyNavbar />
             <MainPage />
           </Route>
           {/* <Route path="/plants">
