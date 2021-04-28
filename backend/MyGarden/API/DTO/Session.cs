2fc035f91bf7d23d45887cee1f271640b503f44f
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-28 09:18:34 +0200

    Create Session model to frontend

---
+++ backend/MyGarden/API/DTO/Session.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGarden.API.DTO
+{
+    public enum UserStatus
+    {
+        New,
+        Registered,
+        LoggedIn
+    }
+
+    public enum UserType
+    {
+        User,
+        Admin
+    }
+
+    public class ErrorModel
+    {
+        public string Type { get; set; } = "";
+        public string Code { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+  
+
+    public class Session
+    {
+
+        public UserStatus Status { get; set; } = UserStatus.New;
+        public UserType UserType { get; set;} = UserType.User;
+        public string Username { get; set; } = "";
+        public bool Success { get; set; } = false;
+        public string Token { get; set; } = "";
+        public ErrorModel Error { get; set; } = new ErrorModel();
+    }
+}
