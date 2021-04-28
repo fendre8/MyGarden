9cb111e077e9e638f1e81ec87750dde507ea2e8c
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-04-28 09:21:21 +0200

    Update MainPage.tsx

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { /*useContext,*/ useEffect, useState } from 'react'
+import { Container, createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
+import MyNavbar from '../Components/MyNavbar';
+import PlantCard from '../Components/PlantCard';
+import { /*PlantsContext,*/ PlantsProvider } from '../Components/PlantsContext';
+import Plant from '../Models/Plant/Plant';
+import { Route, Switch } from 'react-router-dom';
+import { PlantDetails } from '../Components/PlantDetails';
 
-const MainPage = () => {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        grid: {
+            flexGrow: 1,
+        },
+        root: {
+            background: "rgb(232,232,232)",
+        }
+    }),
+);
 
-    return (<>
-        <h2>Main Page</h2>
-    </>
+function MainPage() {
+    const classes = useStyles();
+
+
+    //const plantsContent = useContext(PlantsContext);
+
+    const [plants, setPlants] = useState<Plant[]>([]);
+    const [search, setSearch] = useState("");
+
+    const showResult = (plants: Plant[] | undefined, search: string) => {
+        if (plants !== undefined) {
+            setPlants(plants);
+            setSearch(search);
+        }
+    }
+
+    useEffect(() => {
+    }, [plants])
+
+    return (
+        <div className={classes.root}>
+            <PlantsProvider>
+                <MyNavbar sendSearchResult={showResult} />
+                <Container maxWidth="lg">
+                    {plants.length !== 0 && <h2>Search result for {search}: {plants.length}</h2>}
+                    <Grid container className={classes.grid} spacing={2}>
+                        <Grid item xs={12}>
+                            <Grid container justify="center" spacing={3} >
+                                {plants.map(plant => (
+                                    <Grid key={plant.id} item>
+                                        <PlantCard plant={plant} />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </PlantsProvider>
+            {plants.map(plant => (
+                <Switch key={plant.id} >
+                    <Route path={`/plant/${plant.name.replace(" ","-")}`}>
+                        <PlantDetails />
+                    </Route>
+                </Switch>
+            ))}
+        </div>
     );
 }
 
