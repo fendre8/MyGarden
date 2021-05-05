3060b6651b5d46aaa2fe147545c6d4ec07c0c6e9
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:36:40 +0200

    Update ApplicationUser.cs

--- backend/MyGarden/DAL/EF/DbModels/ApplicationUser.cs
+++ backend/MyGarden/DAL/EF/DbModels/ApplicationUser.cs
@@ -18,6 +18,7 @@ namespace MyGarden.DAL.EF.DbModels
         public virtual ICollection<Friendship> FriendshipFrom { get; set; } = new List<Friendship>();
         //Friendship to me
         public virtual ICollection<Friendship> FriendshipTo { get; set; } = new List<Friendship>();
+
         public virtual ICollection<Plant> Plants { get; set; } = new List<Plant>();
         public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
     }
