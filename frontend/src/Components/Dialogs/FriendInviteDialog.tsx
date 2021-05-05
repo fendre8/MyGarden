f8df29ea4ea2a50d3ef9c4ab9b05e24a212c95b1
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:39:02 +0200

    Create FriendInviteDialog.tsx

---
+++ frontend/src/Components/Dialogs/FriendInviteDialog.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@material-ui/core";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@material-ui/core";
+import React from "react";
+import { useAuth } from "../../http/Auth/auth-context";
+import { inviteFriend } from "../../http/ProfileDataLoader";
+
+type DialogProps = {
+    title: string,
+    openDialog: boolean,
+    setOpenDialog: () => void
+};
+
+export const FriendInviteDialog = (props: DialogProps) => {
+    const auth = useAuth();
+
+    //const [open, setOpen] = React.useState(false);
+    const [friendName, setfriendName] = React.useState("");
+
+    const handleClose = () => {
+        props.setOpenDialog();
+    };
+
+    const handleNewFriendInvite = async () => {
+        handleClose();
+        if (auth.user) {
+            const friendship = await inviteFriend(auth.user.username, friendName);
+        }
+    }
+
+    return (
+        <div>
+            <Dialog open={props.openDialog} onClose={handleClose} aria-labelledby="form-dialog-title" maxWidth="md">
+                <DialogTitle id="form-dialog-title">{props.title}</DialogTitle>
+                <DialogContent dividers>
+                    <DialogContentText>
+                        Please enter your friend name
+                  </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="plant"
+                        label="Username"
+                        type="none"
+                        fullWidth
+                        onChange={ (e) => setfriendName(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                  </Button>
+                    <Button onClick={handleNewFriendInvite} color="primary">
+                        Add
+                  </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+};
