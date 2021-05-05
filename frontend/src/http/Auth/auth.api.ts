6e58f4a5f9c2a37397196f1d82488b4d62a67ed0
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:40:19 +0200

    Update auth.api.ts

--- frontend/src/http/Auth/auth.api.ts
+++ frontend/src/http/Auth/auth.api.ts
@@ -1,24 +1,33 @@
 import axios from 'axios';
 import { ErrorModel } from '../../Models/common/ErrorModel';
 import { Session, UserStatus, UserType } from '../../Models/User/Session';
+import User from '../../Models/User/User';
 import { LoginRequest, RegisterRequest } from '../../Models/User/UserRequests';
-import { baseUrl, headers } from '../SettingsLoader';
+import { baseUrl } from '../SettingsLoader';
 
-export const onLogin = async (data: LoginRequest): Promise<Session> => {
+export interface AuthenticateResult {
+    user: User;
+    token: string;
+}
+
+export async function getCurrentUser(): Promise<Session> {
+    const response = await JSON.parse(localStorage.getItem("session")!) as Session;
+    return response;
+}
+
+export async function login(data: LoginRequest): Promise<Session> {
     return await axios
-        .post<Session>(baseUrl + "auth/login", data, {
-            headers: headers
-        })
+        .post<Session>(baseUrl + "auth/login", data)
         .then(response => {
             axios.defaults.headers.common["Authorization"] =
                 "Bearer " + response.data.token;
 
-            const data = new Session ();
+            const data = new Session();
             data.status = UserStatus.LoggedIn;
             data.succes = true;
             data.token = response.data.token;
             data.userType = UserType.User;
-            data.username = response.data.username;
+            data.user = response.data.user;
             return data;
         })
         .catch(error => {
@@ -41,28 +50,10 @@ export const onLogin = async (data: LoginRequest): Promise<Session> => {
         })
 }
 
-// export const onRegister = async (data: RegisterRequest) => {
-//     const requestConfig: AxiosRequestConfig = {
-//         method: 'post',
-//         url: process.env.REACT_APP_API_BASE_URL + '/auth/register',
-//         headers: headers,
-//         data
-//     }
-//     try {
-//         const { data: response } = await axios.request(requestConfig);
-//         console.log(response);
-//     } catch (e) {
-//         console.error(e.response);
-//         return { error: e.response.data.message }
-//     }
-// }
-
-export const onRegister = async (
-    data: RegisterRequest
-): Promise<ErrorModel | undefined> => {
+export async function register(data: RegisterRequest): Promise<ErrorModel | undefined> {
     var url = baseUrl + "auth/register";
     return await axios
-        .post<ErrorModel>(url, data, { headers: headers })
+        .post<ErrorModel>(url, data)
         .then(response => {
             return undefined;
         })
@@ -81,3 +72,11 @@ export const onRegister = async (
                 };
         });
 };
+
+const exported = {
+    login,
+    register,
+    getCurrentUser
+};
+
+export default exported;
