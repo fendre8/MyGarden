672a6bf631edf261786e2594ff58c67306e7ffe6
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:35:57 +0200

    Update mapping

--- backend/MyGarden/API/DTO/Mapping/MappingProfile.cs
+++ backend/MyGarden/API/DTO/Mapping/MappingProfile.cs
@@ -7,6 +7,18 @@ namespace MyGarden.Models.Mapping
         public MappingProfile()
         {
             CreateMap<DAL.EF.DbModels.ApplicationUser, Profile>();
+            CreateMap<Profile, DAL.EF.DbModels.ApplicationUser>()
+                .ForMember(dest => dest.FriendshipFrom, opt => opt.MapFrom(src => src.Friends))
+                .ForMember(dest => dest.FriendshipTo, opt => opt.MapFrom(src => src.Friends));
+
+            CreateMap<DAL.EF.DbModels.Plant, Plant>();
+            CreateMap<Plant, DAL.EF.DbModels.Plant>();
+
+            //CreateMap<DAL.EF.DbModels.Friendship, FriendshipDTO>();
+            //CreateMap<FriendshipDTO, DAL.EF.DbModels.Friendship>();
+
+            CreateMap<Issue, DAL.EF.DbModels.Issue>();
+            CreateMap<DAL.EF.DbModels.Issue, Issue>();
         }
     }
 }
