5c221e8e0067a9b4b619aca533d6ec7b394ef57c
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:39:08 +0200

    Create NewPlantDialog.tsx

---
+++ frontend/src/Components/Dialogs/NewPlantDialog.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@material-ui/core";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@material-ui/core";
+import React from "react";
+import { useAuth } from "../../http/Auth/auth-context";
+import { addNewPlant } from "../../http/PlantDataLoader";
+
+type DialogProps = {
+    title: string,
+    openDialog: boolean,
+    setOpenDialog: () => void
+};
+
+export const NewPlantDialog = (props: DialogProps) => {
+    const auth = useAuth();
+
+    //const [open, setOpen] = React.useState(false);
+    const [plantName, setPlantName] = React.useState("");
+    const [plantTime, setPlantTime] = React.useState("");
+
+    const handleClose = () => {
+        props.setOpenDialog();
+    };
+
+    const handleNewPlant = async () => {
+        handleClose();
+        const plant = await addNewPlant(auth.user!.username, plantName)
+    }
+
+    return (
+        <div>
+            <Dialog open={props.openDialog} onClose={handleClose} aria-labelledby="form-dialog-title" maxWidth="md">
+                <DialogTitle id="form-dialog-title">{props.title}</DialogTitle>
+                <DialogContent dividers>
+                    <DialogContentText>
+                        Please enter the plant name
+                  </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="plant"
+                        label="Plant name"
+                        type="none"
+                        fullWidth
+                        onChange={ (e) => setPlantName(e.target.value)}
+                    />
+                    <TextField
+                        id="date"
+                        label="Plant time"
+                        type="date"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        onChange={ (e) => setPlantTime(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                  </Button>
+                    <Button onClick={handleNewPlant} color="primary">
+                        Add
+                  </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+};
