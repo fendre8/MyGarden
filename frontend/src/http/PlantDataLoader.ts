d0e72aca2d5f214c4da08f957761d5e3226d04d0
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-05 20:40:24 +0200

    Update PlantDataLoader.ts

--- frontend/src/http/PlantDataLoader.ts
+++ frontend/src/http/PlantDataLoader.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { ErrorModel } from "../Models/common/ErrorModel";
 import Plant from "../Models/Plant/Plant";
 import { baseUrl, headers } from "./SettingsLoader";
 
 
 export const getPlants = async (plantName: string): Promise<Plant[] | undefined> => {
     return await axios
-        .get(baseUrl + "plant/" + plantName, {
+        .get(baseUrl + "plant/name/" + plantName, {
             headers: headers
         })
         .then(response => {
@@ -17,3 +16,42 @@ export const getPlants = async (plantName: string): Promise<Plant[] | undefined>
             return undefined;
         })
 };
+
+export const getUserPlants = async (username: string): Promise<Plant[]> => {
+    return await axios
+        .get(baseUrl + "profiles/name/" + username + "/plants", 
+        { headers: headers })
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => {
+            console.log("Nincs növényed");
+            return undefined;
+        });
+};
+
+export const getPlantById = async (id: number): Promise<Plant> => {
+    return await axios
+        .get(baseUrl + "plant/id/" + id, 
+        { headers: headers})
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => {
+            console.log(`Nincs ${id}-s növény`);
+            return err;
+        });
+};
+
+export const addNewPlant = async (username: string, plant: string): Promise<Plant> => {
+    return await axios
+        .post(baseUrl + "profiles/name/" + username + "/plant",{ plantName : plant },
+        { headers: headers })
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(err => {
+            console.log("Nem sikerült feltölteni");
+        })
+}
