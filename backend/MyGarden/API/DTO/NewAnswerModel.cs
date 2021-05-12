56dd38873fd3f01130bf5d22695f21a812a28ec3
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-12 09:25:15 +0200

    Create NewAnswerModel.cs

---
+++ backend/MyGarden/API/DTO/NewAnswerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGarden.API.DTO
+{
+    public class NewAnswerModel
+    {
+        public int userId { get; set; }
+        public string answerText { get; set; }
+    }
+}
