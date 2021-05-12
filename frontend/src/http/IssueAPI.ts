d0fd7d14711a038e071f519e23971ee9bd5ab540
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-12 09:29:52 +0200

    Update IssueAPI.ts

--- frontend/src/http/IssueAPI.ts
+++ frontend/src/http/IssueAPI.ts
@@ -1,14 +1,55 @@
 import axios from "axios";
 import Issue from "../Models/Issue/Issue";
-import { baseUrl } from "./SettingsLoader";
+import NewIssueData from "../Models/Issue/NewIssueData";
+import { baseUrl, headers } from "./SettingsLoader";
 
-export async function getAll() {
-    const response = await axios.get<Issue[]>(baseUrl + "issues");
-    return response.data;
+export const getAllIssues = async (): Promise<Issue[]> => {
+    return await axios
+        .get(baseUrl + "issue",
+            { headers: headers })
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+export const addNewIssue = async (data: NewIssueData): Promise<Issue> => {
+    console.log(data);
+    return await axios
+        .post(baseUrl + "issue", data,
+            { headers: headers })
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+export async function getIssueById(id: number): Promise<Issue> {
+    return await axios.get(baseUrl + "issue/id/" + id)
+        .then(res => {
+            return res.data;
+        })
+        .catch(err => {
+            console.log("Issue not found", err);
+        });
+}
+
+export async function toggleIssueIsOpen(id: number) {
+    return await axios.put(baseUrl + "issue/id/" + id + "/isopen")
+        .then(res => {
+            return res.data;
+        })
+        .catch(err => {
+            console.log("Issue not found", err);
+        });
 }
 
 export async function add(issue: Issue) {
-    issue.id = 0;
     const response = await axios.post<Issue>(baseUrl + "issue", issue);
     return response.data;
 }
@@ -19,7 +60,10 @@ export async function remove(id: number) {
 }
 
 const exported = {
-    getAll,
+    getAllIssues,
+    addNewIssue,
+    getIssueById,
+    toggleIssueIsOpen,
     add,
     remove
 };
