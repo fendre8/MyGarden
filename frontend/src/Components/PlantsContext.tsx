57ce42e32373e94d03065bd9e7d6e1380718975c
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-12 09:29:32 +0200

    Update PlantsContext.tsx

--- frontend/src/Components/PlantsContext.tsx
+++ frontend/src/Components/PlantsContext.tsx
@@ -2,23 +2,39 @@ import React, { createContext, useState } from "react";
 import Plant from "../Models/Plant/Plant";
 
 type PlantContent = {
-    _plants: Plant[];
-    _setPlants(_plants: Plant[]): void;
+    plants: Plant[];
+    setPlants(_plants: Plant[]): void;
 }
 
 export const PlantsContext = createContext<PlantContent | undefined>(undefined)
 
-export const PlantsProvider: React.FC = ({ children }) => {
-    const [plants, setPlants] = useState<Plant[]>([])
+export const PlantsProvider = (props: { children: React.ReactChild | React.ReactFragment }) => {
+    const [plants, setPlants] = useState<Plant[]>([
+        // { 
+        //     id : 1,
+        //     description: "",
+        //     first_harvest_exp: "1",
+        //     height: 1,
+        //     img_url: "",
+        //     last_harvest_exp: "",
+        //     median_lifespan: "",
+        //     name: "asd",
+        //     row_spacing: 0,
+        //     scientific_name: "",
+        //     sowing_method: "",
+        //     spread: 0,
+        //     sun_requirements: "",
+        // }
+    ])
 
     const plantContent : PlantContent = {
-        _plants: plants,
-        _setPlants: setPlants
+        plants: plants,
+        setPlants: setPlants
     };
 
     return (
         <PlantsContext.Provider value={plantContent} >
-            {children}
+            {props.children}
         </PlantsContext.Provider>
     )
 }
