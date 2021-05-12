1067d6d973f6cdddcea2fb18f714713683f3b4ef
fendre8 <58511574+fendre8@users.noreply.github.com>
2021-05-12 09:29:39 +0200

    Update AuthenticatedRoute.tsx

--- frontend/src/Components/Routes/AuthenticatedRoute.tsx
+++ frontend/src/Components/Routes/AuthenticatedRoute.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, RouteProps } from "react-router";
 import { Route } from "react-router-dom";
 import { useAuth } from "../../http/Auth/auth-context";
+import { Session } from "../../Models/User/Session";
 
 
 export function AuthenticatedRoute({ ...props }: RouteProps) {
-    const { user } = useAuth();
+    const session = JSON.parse(localStorage.getItem("session")!) as Session;
 
-    if (user === undefined) return <Redirect to="/login" />
+    if (session === null) return <Redirect to="/login" />
 
     return <Route {...props} />
 }
